a4deb4cd1c8198e22abcb39b1ac57c3fb2874cd2
Simeon Florea <simeon.florea2000@gmail.com>
2020-11-13 18:56:05 -0800

    Initial Commit of jp athleet API in ASP.NET: Model for DB created. Working on implementing DBContext.

---
+++ AthleetAPI/Models/DataModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace AthleetAPI.Models
+{
+    public class UserModel
+    {
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string userHeadline { get; set; }
+    }
+
+    public class UserWorkoutModel
+    {
+        public int userWorkoutId { get; set; }
+        public int userId { get; set; }
+        public int workoutId { get; set; }
+        public DateTime workoutDate { get; set; }
+    }
+
+    public class UserWorkoutExercisesModel
+    {
+        public int userWorkoutExerciseId { get; set; }
+        public int userWorkoutId { get; set; }
+        public int exerciseId { get; set; }
+        public int numberOfReps { get; set; }
+    }
+
+    public class WorkoutsModel
+    {
+        public int workoutId { get; set; }
+        public string workoutName { get; set; }
+        public string description { get; set; }
+    }
+
+    public class WorkoutExercisesModel
+    {
+        public int workoutExerciseId { get; set; }
+        public int workoutId { get; set; }
+        public int exerciseId { get; set; }
+        public int defaultReps { get; set; }
+    }
+
+    public class ExercisesModel
+    {
+        public int exerciseId { get; set; }
+        public string exerciseName { get; set; }
+        public string description { get; set; }
+        public int DefaultReps { get; set; }
+    }
+}
