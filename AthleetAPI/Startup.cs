225ecec673ceab84cdb2f688680afce97a43df6e
Simeon Florea <simeon.florea2000@gmail.com>
2020-11-15 23:28:27 -0800

    Added connection string into appsettings.json - later will be moved into secret storage using .NET CoreCLI

--- AthleetAPI/Startup.cs
+++ AthleetAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AthleetAPI.Models;
 
 namespace AthleetAPI
 {
@@ -25,6 +27,8 @@ namespace AthleetAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AthleetContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AthleetDatabase")));       
             services.AddControllers();
         }
 
