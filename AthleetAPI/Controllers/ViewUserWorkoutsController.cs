c3d7e842def9d24100338a7b0dd1da158b9743a8
Ryan Rupert <mudge25+code@gmail.com>
2020-12-04 22:24:12 -0800

    Add source ViewUserWorkoutsController.cs
    This adds the ViewUserWorkoutsController and the ViewWorkout method
    which will call the SQL function fnViewUserWorkouts.

---
+++ AthleetAPI/Controllers/ViewUserWorkoutsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AthleetAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Data.SqlClient;
+
+namespace AthleetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViewUserWorkoutsController : ControllerBase
+    {
+        private readonly AthleetContext _context;
+
+        public ViewUserWorkoutsController(AthleetContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<ViewUserWorkouts>>> ViewWorkout()
+        {
+            return _context.ViewUserWorkouts.FromSqlRaw("select * from fnViewUserWorkouts('SimiF')").ToList();
+        }
+    }
+}
