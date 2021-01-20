b2e14c850e8db9191ea53848db9022aff98445dd
Simeon Florea <simeon.florea2000@gmail.com>
2021-01-19 21:01:30 -0800

    Divided Data and Group models to improve the API

---
+++ AthleetAPI/Models/Group Models/GroupModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AthleetAPI.Models
+{
+    public class UserGroup
+    {
+
+    }
+
+    public class WorkoutGroup
+    {
+
+    }
+    public class ExercisesGroup
+    {
+
+    }
+}
