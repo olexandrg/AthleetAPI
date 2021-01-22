d35823a0bf8dfaf130dec1e40ac46ea4bf73b255
Ryan Rupert <ryan.rupert@oit.edu>
2021-01-22 14:35:52 -0800

    Modify TeamController.cs
    This adds the bones of GET and untested endpoint for POST to create a
    team.

--- AthleetAPI/Controllers/TeamController.cs
+++ AthleetAPI/Controllers/TeamController.cs
@@ -22,6 +22,39 @@ namespace AthleetAPI.Controllers
             _context = context;
         }
 
+        //GET: api/Team
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult> GetTeam([FromHeader(Name = "Authorization")] String token, [FromQuery(Name = "teamName")] String teamName)
+        { 
+            //list all the teams the user is part of
+            String UID = Utilities.pullUID(token);
+
+            var uid = new SqlParameter("@UID", UID);
+            var name = new SqlParameter("@Name", teamName);
+
+            await _context.Database.ExecuteSqlRawAsync("EXEC procDeleteTeam @UID, @Name", name, uid);
+
+            //TODO: return the actual data
+            return StatusCode(200);
+        }
+
+        //POST: api/Team
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult> CreateTeam([FromHeader(Name = "Authorization")] String token, [FromQuery(Name = "teamName")] String teamName, [FromQuery(Name = "description")] String description)
+        {
+             String UID = Utilities.pullUID(token);
+
+            var uid = new SqlParameter("@UID", UID);
+            var name = new SqlParameter("@Name", teamName);
+            var Description = new SqlParameter("@Description", description);
+
+            await _context.Database.ExecuteSqlRawAsync("EXEC procInsertNewTeam @UID, @Name, @Description", name, uid, Description);
+
+            return StatusCode(201);           
+        }
+
         // DELETE: api/Team
         [HttpDelete]
         [Authorize]
