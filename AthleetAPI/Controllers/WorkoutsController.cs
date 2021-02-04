3339976a6fb37e44e9c338712e9ac3efb8462f8e
Ryan Rupert <ryan.rupert@oit.edu>
2021-02-04 15:56:25 -0800

    Refator WorkoutsController.cs
    This removes old unneeded methods and updates the CreateWorkout method.

--- AthleetAPI/Controllers/WorkoutsController.cs
+++ AthleetAPI/Controllers/WorkoutsController.cs
@@ -22,18 +22,10 @@ namespace AthleetAPI.Controllers
             _context = context;
         }
 
-        // GET: api/Workouts
-        [HttpGet]
+        // POST: api/Workouts
+        [HttpPost]
         [Authorize]
-        public async Task<ActionResult<IEnumerable<Workouts>>> GetWorkouts()
-        {
-            return await _context.Workouts.ToListAsync();
-        }
-
-        // GET: api/Workouts/InsertWorkout
-        [HttpGet("InsertWorkout")]
-        [Authorize]
-        public async Task<ActionResult<IEnumerable<Workouts>>> CreateWorkout(
+        public async Task<ActionResult> CreateWorkout(
             [FromQuery(Name = "Name")] String Name,
             [FromQuery(Name = "Description")] String Description,
             [FromHeader(Name = "Authorization")] String token)
@@ -52,56 +44,5 @@ namespace AthleetAPI.Controllers
             //return success
             return StatusCode(201);
         }
-
-        // GET: api/Workouts
-        [HttpGet]
-        [Authorize]
-        public async Task<ActionResult<IEnumerable<Workouts>>> DeleteWorkout(
-            [FromQuery(Name = "Name")] String Name,
-            [FromHeader(Name = "Authorization")] String token)
-        {
-            //pull the UID from the token
-            String UID = Utilities.pullUID(token);
-
-            //generate the sql parameters
-            var name = new SqlParameter("@Name", Name);
-            var uid = new SqlParameter("@UID", UID);
-
-            //run the query
-            await _context.Database.ExecuteSqlRawAsync("EXEC procedureDeleteWorkoutForUser @UID, @Name", uid, name);
-
-            //return success
-            return StatusCode(201);
-        }
-
-        // GET: api/Workouts/5
-        [HttpGet("{id}")]
-        [Authorize]
-        public async Task<ActionResult<Workouts>> GetWorkouts(int id)
-        {
-            var workouts = await _context.Workouts.FindAsync(id);
-
-            if (workouts == null)
-            {
-                return NotFound();
-            }
-
-            return workouts;
-        }
-
-        // POST: api/Workouts
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        [Authorize]
-        public async Task<ActionResult<Workouts>> PostWorkouts(Workouts workouts)
-        {
-            _context.Workouts.Add(workouts);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetWorkouts", new { id = workouts.WorkoutId }, workouts);
-        }
-
-    
     }
 }
