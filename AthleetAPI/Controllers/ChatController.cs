45a787794bb2cf23df201d06a0932275b8fd5a80
Ryan Rupert <ryan.rupert@oit.edu>
2021-05-06 17:19:08 -0700

    Add ChatController and get for listing team conversations

---
+++ AthleetAPI/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AthleetAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace AthleetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly AthleetContext _context;
+
+        public ChatController(AthleetContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("team")]
+        [Authorize]
+        public async Task<ActionResult<IEnumerable<Conversation>>> GetTeamConversation(
+                [FromQuery(Name = "teamName")] String teamName
+        )
+        {
+            var TeamName = new SqlParameter("@TeamName", teamName);
+            return _context.Conversation.FromSqlRaw("SELECT * FROM fnViewTeamUsers(@TeamName)", TeamName).ToList();
+        }
+    }
+}
